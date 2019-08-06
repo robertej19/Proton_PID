01b12b94766c339de29aebd754c864db36f314ae
robertej19 <robertej@mit.edu>
2019-08-06 14:32:33 -0400

    "new"

--- protons.groovy
+++ protons.groovy
@@ -15,72 +15,6 @@ import org.jlab.clas.physics.LorentzVector;
 import org.jlab.groot.base.GStyle;
 import org.jlab.groot.graphics.EmbeddedCanvas;
 
-def run = args[0].toInteger()
-
-TDirectory out = new TDirectory()
-out.mkdir('/'+run)
-out.cd('/'+run)
-
-float EB = 10.6f
-if(run>6607) EB=10.2f
-// H1F H_elec_mom[6];
-// H2F H_elec_vz_mom[6], H_elec_theta_mom[6], H_elec_theta_phi[6];
-H_elec_mom =(0..5).collect{
-	def h1 = new H1F("H_elec_mom_S"+(it+1), "H_elec_mom_S"+(it+1),100, 0, EB);
-	return h1
-}
-
-H_elec_vz_mom =(0..<6).collect{
-	def h1 = new H2F("H_elec_vz_mom_S"+(it+1), "H_elec_vz_mom_S"+(it+1),100,0,EB,100,-25,25);
-	return h1
-}
-
-H_elec_theta_mom =(0..<6).collect{
-	def h1 = new H2F("H_elec_theta_mom_S"+(it+1), "H_elec_theta_mom_S"+(it+1),100,0,EB,100,0,40);
-	return h1
-}
-
-H_elec_phi_mom = (0..<6).collect{
-	def h1 = new H2F("H_elec_phi_mom_S"+(it+1), "H_elec_phi_mom_S"+(it+1),100,0,EB,100,-180,180);
-	return h1
-}
-
-H_elec_theta_phi =(0..<6).collect{
-	def h1 = new H2F("H_elec_theta_phi_S"+(it+1), "H_elec_theta_phi_S"+(it+1),100,-180,180,100,0,40);
-	return h1
-}
-
-//H_elec_mom=new H1F("H_elec_mom", "H_elec_mom_S",100, 0, EB);
-
-
-int e_index, e_sect
-float e_mom, e_theta, e_phi, e_vx, e_vy, e_vz
-LorentzVector Ve = new LorentzVector()
-
-filenum=-1
-for (arg in args){
-	filenum=filenum+1
-	if (filenum==0) continue
-	HipoDataSource reader = new HipoDataSource();
-	reader.open(arg);
-
-	while( reader.hasEvent()){
-		DataEvent event = reader.getNextEvent();
-		processEvent(event)
-	}
-}
-
-(0..<6).each{
-	out.addDataSet(H_elec_mom[it])
-	out.addDataSet(H_elec_vz_mom[it])
-	out.addDataSet(H_elec_theta_mom[it])
-	out.addDataSet(H_elec_phi_mom[it])
-	out.addDataSet(H_elec_theta_phi[it])
-}
-
-//out.addDataSet(H_elec_mom)
-out.writeFile('electron_pID_new'+run+'.hipo')
-
 public void processEvent(DataEvent event) {
 	if(!event.hasBank("REC::Particle")) return
 	DataBank partBank = event.getBank("REC::Particle");
@@ -89,20 +23,12 @@ public void processEvent(DataEvent event) {
 	if (e_index>-1){
 		makeElectron(partBank)
 		fillHists()
-		//H_elec_mom.fill(e_mom)
 	}
 	else return;
 }
-
-
 public void fillHists(){
-	H_elec_mom[e_sect-1].fill(e_mom);
-	H_elec_vz_mom[e_sect-1].fill(e_mom,e_vz);
 	H_elec_theta_mom[e_sect-1].fill(e_mom,e_theta)
-	H_elec_phi_mom[e_sect-1].fill(e_mom,e_phi)
-	H_elec_theta_phi[e_sect-1].fill(e_phi,e_theta);
 }
-
 public void makeElectron(DataBank recPart){
 		int ei=e_index
 		float px = recPart.getFloat("px",ei)
@@ -122,7 +48,6 @@ public void makeElectron(DataBank recPart){
 		e_phi = (float) Math.toDegrees(Ve.phi())
 		e_theta = (float) Math.toDegrees(Ve.theta())
 }
-
 public boolean hasElectron(DataBank recPart){
 	boolean found = false
 	for(int p=0;p<recPart.rows();p++){
@@ -133,7 +58,6 @@ public boolean hasElectron(DataBank recPart){
 	}
 	return found
 }
-
 public boolean isElectron(DataBank recPart, int p){
 	if (ele_default_PID_cut(recPart,p)&& ele_charge_cut(recPart,p) && ele_kine_cut(recPart,p)&& inDC(recPart,p)){
 		//System.out.println("Electron Found!")
@@ -142,24 +66,19 @@ public boolean isElectron(DataBank recPart, int p){
 	}
 	else return false
 }
-
 public boolean inDC(DataBank recPart, int p){
 	int status = recPart.getShort("status", p);
 	if (status<0) status = -status;
 	boolean inDC = (status>=2000 && status<4000);
 }
-
-
 public boolean ele_default_PID_cut(DataBank recPart, int p){
   if(recPart.getInt("pid",p)==11) return true;
   else return false;
 }
-
 public boolean ele_charge_cut(DataBank recPart, int p){
   if(recPart.getInt("charge",p)==-1) return true;
   else return false;
 }
-
 public boolean ele_kine_cut(DataBank recPart, int p){
 	float px = recPart.getFloat("px", p);
 	float py = recPart.getFloat("py", p);
@@ -173,3 +92,45 @@ public boolean ele_kine_cut(DataBank recPart, int p){
 	}
 	else return false;
 }
+
+"""Start of actual program"""
+
+"""xxxxxxxxxxxxxxxxxxxx Variable definitions xxxxxxxxxxxxxxxxxxxxxxxxxx"""
+def run = args[0].toInteger()
+float EB = 10.6f
+if(run>6607) EB=10.2f
+
+int e_index, e_sect
+float e_mom, e_theta, e_phi, e_vx, e_vy, e_vz
+LorentzVector Ve = new LorentzVector()
+
+TDirectory out = new TDirectory()
+out.mkdir('/'+run)
+out.cd('/'+run)
+
+H_elec_theta_mom =(0..<6).collect{
+	def h1 = new H2F("H_elec_theta_mom_S"+(it+1), "H_elec_theta_mom_S"+(it+1),100,0,EB,100,0,40);
+	return h1
+}
+
+"""xxxxxxxxxxxxxxxxxxxx ------------------ xxxxxxxxxxxxxxxxxxxxxxxxxx"""
+
+filenum=-1
+for (arg in args){
+	filenum=filenum+1
+	if (filenum==0) continue
+	HipoDataSource reader = new HipoDataSource();
+	reader.open(arg);
+
+	while( reader.hasEvent()){
+		DataEvent event = reader.getNextEvent();
+		processEvent(event)
+	}
+}
+
+(0..<6).each{
+	out.addDataSet(H_elec_theta_mom[it])
+}
+
+//out.addDataSet(H_elec_mom)
+out.writeFile('electron_pID_new'+run+'.hipo')
