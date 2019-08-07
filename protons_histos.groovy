69ace05766259f1b7782b7a43fe1b6397bd5e5b3
robertej19 <robertej@mit.edu>
2019-08-07 11:00:18 -0400

    ""

--- protons_histos.groovy
+++ protons_histos.groovy
@@ -10,19 +10,19 @@ import org.jlab.groot.graphics.EmbeddedCanvas;
 for(arg in args) {
   TDirectory dir = new TDirectory()
   dir.readFile(arg)
-  def h_1 = dir.getObject(String.format('/5038'))
+  def h_1 = dir.getObject(String.format('/5038/H_proton_beta_momentum_S3'))
   //...
 //
 //  def h_n = dir.getObject(String.format('/path/to/histogram_n'))
 
   EmbeddedCanvas canvas  = new EmbeddedCanvas();
   canvas.setSize(3500,1500);
-  canvas.divide(sqrt(n),sqrt(n));
+  //canvas.divide(sqrt(n),sqrt(n));
   canvas.setAxisTitleSize(18);
   canvas.setAxisFontSize(18);
   canvas.setTitleSize(18);
   canvas.cd(0);canvas.draw(h1);
   //...
   //canvas.cd(n-1);canvas.draw(h_n);
-  canvas.save(“dumped_histogram”+arg+".pdf")
+  canvas.save('dumped_histogram'+arg+'.pdf')
 }
