02ab2ce9cf9d8fa756a471f6ff68490c83a251d7
robertej19 <robertej@mit.edu>
2019-08-08 16:07:48 -0400

    ""

--- protons.groovy
+++ protons.groovy
@@ -38,7 +38,9 @@ public void processEvent(DataEvent event) {
 		(p_momentum, beta_recon,p_theta,p_phi,p_vz,beta_calc) = makeParticle(reconstructedParticle,p_ind)
 
 		DataBank recon_Scint = event.getBank("REC::Scintillator");
-		println(recon_Scint.getFloat("index",p_ind))
+		println("Index is: "+recon_Scint.getShort("index",p_ind))
+		println("Detector is: "+recon_Scint.getShort("detector",p_ind))
+		println("Layer is: "+recon_Scint.getShort("layer",p_ind))
 
 		fillHists(p_momentum,beta_recon,p_theta,p_phi,p_vz,beta_calc)
 	}
