ef6451e948e06cc21c460b9166886bb84d0ff7fc
robertej19 <robertej@mit.edu>
2019-08-13 12:52:16 -0400

    ""

--- protons.groovy
+++ protons.groovy
@@ -187,7 +187,7 @@ def hist_brbc = [:].withDefault{new H1F("hist_${it}", "title for ${it}",100,-1,1
 //H_beta_recon_beta_calc =(0..<max_hists).collect{new H1F("H_beta_recon_beta_calc_S"+(it+1), "H_beta_recon_beta_calc_S"+(it+1),100, -1, 1)}
 for(int isec=1;isec<=6;isec++)
  for(int ilay=1;ilay<=3;ilay++)
-   hist_brbc["sec${isec}_layer${ilay}"].fill()
+   hist_brbc["sec${isec}_layer${ilay}"].fill(0)
 
 
 
